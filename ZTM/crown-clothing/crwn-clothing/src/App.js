5ed3bf649b5480133d4d736c94bd8f82c8961d9f
jonathanoleaz <jonathanoleaz@hotmail.com>
2021-08-14 12:55:54 -0500

    adding header, colletion item and collection preview components

--- ZTM/crown-clothing/crwn-clothing/src/App.js
+++ ZTM/crown-clothing/crwn-clothing/src/App.js
@@ -2,23 +2,20 @@
 import './App.css';
 //import './homepage.component';
 import HomePage from './pages/homepage/homepage.component';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
+import {Switch, Route} from 'react-router-dom';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/shop/hats' component={HatsPage}/>
-      </Switch>
-      </BrowserRouter>
+      <Header/>
+      
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/shop' component={ShopPage}/>
+        </Switch>
+      
     </div>
   );
 }
