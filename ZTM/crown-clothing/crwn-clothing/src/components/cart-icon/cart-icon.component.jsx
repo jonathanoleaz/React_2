66fd6812aae0b2591501985cef3779bce277a7fd
jonathanoleaz <jonathanoleaz@hotmail.com>
2021-08-21 20:57:04 -0500

    adding redux state selectors in cart-icon and cart-dropdown components to improve performance and use memorization

--- ZTM/crown-clothing/crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ ZTM/crown-clothing/crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
@@ -18,13 +19,17 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-/** example of a state selector*/
+/** example of a state selector but is inefficient beacuse the calculation is done each time the state changes
 const mapStateToProps = ( { cart: {cartItems} } ) =>(
     {
         itemCount: cartItems.reduce(
             (accumulatedQuantity, cartItem) => accumulatedQuantity+cartItem.quantity, 0
             )
     }
-)
+)*/
+
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
