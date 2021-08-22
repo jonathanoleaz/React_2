c225d8a41d4dc3bf95f80fcddab98f182e69131e
jonathanoleaz <jonathanoleaz@hotmail.com>
2021-08-22 00:35:25 -0500

    adding add item and remove item functioonality in checkout-items

--- ZTM/crown-clothing/crwn-clothing/src/redux/cart/cart.utils.js
+++ ZTM/crown-clothing/crwn-clothing/src/redux/cart/cart.utils.js
@@ -13,4 +13,27 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     /*in this part we initialize the quantity prop of each cartItem */
     return [...cartItems, {...cartItemToAdd, quantity: 1}]
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id 
+    );
+
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(
+        cartItem =>
+        cartItem.id === cartItemToRemove.id 
+        ? 
+            {
+                ...cartItem,
+                quantity: cartItem.quantity-1
+            }
+        :
+        cartItem
+    )
+
 }
