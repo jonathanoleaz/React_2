b1d43830d3fb0f706a96c7257949b0d051d59b0d
jonathanoleaz <jonathanoleaz@hotmail.com>
2021-08-23 10:29:09 -0500

    changing directory component to use redux

--- ZTM/crown-clothing/crwn-clothing/src/redux/root-reducer.js
+++ ZTM/crown-clothing/crwn-clothing/src/redux/root-reducer.js
@@ -5,6 +5,7 @@ import { combineReducers } from "redux";
 
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
 /** 'key' is at which point of the store we want to start persisting,
  * 'whitelist' s an array of the store items we want to persist
@@ -17,7 +18,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
