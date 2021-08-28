8db9a76ee4ee90ce48f5ec00daae9bff05901672
jonathanoleaz <jonathanoleaz@hotmail.com>
2021-08-28 13:41:11 -0500

    setting redux logger

--- ZTM/crown-clothing/crwn-clothing/src/redux/store.js
+++ ZTM/crown-clothing/crwn-clothing/src/redux/store.js
@@ -5,7 +5,11 @@ import { applyMiddleware, createStore } from "redux";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
