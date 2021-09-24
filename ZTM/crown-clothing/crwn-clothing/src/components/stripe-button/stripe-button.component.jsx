fdd5bd96156ae7fb772569ca14ccdf4082822615
jonathanoleaz <jonathanoleaz@hotmail.com>
2021-09-24 14:18:47 -0500

    separating into client and server folders, adding express server to simulate payment in stripe

--- ZTM/crown-clothing/crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
+++ ZTM/crown-clothing/crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
-const onToken = token =>{
-    console.log(token);
-}
+
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51JSMenFnm1EFTo7stgBejN1CyhDQBYjh5O6f5LpL7AoIFqesIXflm138G0dNYhhH6xWGn5wvscIW5nHdJpagKbb200chKtLK1C';
+
+    const onToken = token =>{
+        axios({
+            url: 'payment',
+            method: 'post',
+            data:{
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment was successful')
+        }).catch(error => {
+            console.log('Payment error', JSON.parse(error));
+            alert('There was an issue with your payment');
+        })
+    }
 
     return (
         <StripeCheckout
