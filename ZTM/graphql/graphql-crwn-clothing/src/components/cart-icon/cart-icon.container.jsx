d04cc123aa1c74c3e1a04f050e3b8388d7669182
jonathanoleaz <jonathanoleaz@hotmail.com>
2021-09-26 19:35:32 -0500

    Changing to flowRight (compose) style to specify the mutation and query of CartIcon

--- ZTM/graphql/graphql-crwn-clothing/src/components/cart-icon/cart-icon.container.jsx
+++ ZTM/graphql/graphql-crwn-clothing/src/components/cart-icon/cart-icon.container.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Mutation, Query } from "react-apollo";
+import { compose, graphql } from "react-apollo";
 import {gql} from 'apollo-boost';
+import { flowRight } from 'lodash';
 
 import CartIcon from "./cart-icon.component";
 
@@ -15,16 +16,13 @@ const GET_ITEM_COUNT = gql`
     itemCount @client
 }`
 
-const CartIconContainer = () => (
-    <Query query={GET_ITEM_COUNT}>
-        {
-        ({data: {itemCount}}) => (<Mutation mutation={TOGGLE_CART_HIDDEN}>
-            {
-                toggleCartHidden => <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />
-            }
-        </Mutation>)
-        }
-    </Query>
-);
+const CartIconContainer = ({ data: { itemCount }, toggleCartHidden}) => {
+    return(
+    <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />   
+    )
+};
 
-export default CartIconContainer;
+export default flowRight(
+    graphql(GET_ITEM_COUNT),
+    graphql(TOGGLE_CART_HIDDEN, {name: 'toggleCartHidden'})
+)(CartIconContainer);
